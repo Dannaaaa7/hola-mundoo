705baf9052963c04dbe4884a2c9ad1da7ebe5686
jesuss12 <87486974+jesuss12@users.noreply.github.com>
2021-07-15 12:21:09 -0500

    Create .js

---
+++ .js
@@ -0,0 +1,16 @@
+# hola-mundoo
+public class HolaMundo {
+
+}
+public class HolaMundo {
+	public static void main(String[] args) {		
+
+	}
+}
+public class HolaMundo {
+
+	public static void main(String[] args) {		
+		System.out.println("Hola Mundo");
+	}
+
+}
